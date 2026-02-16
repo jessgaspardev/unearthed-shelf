b67df36fc0bb920bc1081db423065414739993c6
Jess <jessgaspardev@gmail.com>
2026-02-16 11:20:50 +0000

    refactor: create dynamic routes for category pages

---
+++ src/types/genres.ts
@@ -0,0 +1,4 @@
+export type Genre = 
+  "Aliens" | "Alternate History" | "Apocalyptic" | "Cyberpunk" | 
+  "Dystopian" | "Hard Sci-Fi" | "Military" | "Robots" | 
+  "Space Opera" | "Steampunk" | "Time Travel";
